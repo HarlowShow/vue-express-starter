60cdca229a877f0656e963112bfdb854e752547d
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2023-07-19 18:19:12 +0100

    add basic CRUD operations

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from "vue";
+import { remult } from "remult";
+import { Task } from "./shared/Task";
+
+const taskRepo = remult.repo(Task);
+const tasks = ref<Task[]>([]);
+const newTaskTitle = ref("")
+async function addTask() {
+  try {
+    const newTask = await taskRepo.insert({ title: newTaskTitle.value })
+    tasks.value.push(newTask)
+    newTaskTitle.value = ""
+  } catch (error: any) {
+    alert(error.message)
+  }
+}
+
+async function saveTask(task: Task) {
+  try {
+    await taskRepo.save(task)
+  } catch (error: any) {
+    alert(error.message)
+  }
+}
+
+async function deleteTask(task: Task) {
+  try {
+    await taskRepo.delete(task)
+    tasks.value = tasks.value.filter(t => task !== t)
+  } catch (error: any) {
+    alert(error.message)
+  }
+}
+
+onMounted(() =>
+  taskRepo
+    .find({
+      limit: 20,
+      orderBy: { createdAt: "asc" }
+      //where: { completed: true }
+    })
+    .then(items => (tasks.value = items))
+)
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <h1>todos</h1>
+    <main>
+      <form @submit.prevent="addTask()">
+        <input v-model="newTaskTitle" placeholder="What needs to be done?" />
+        <button>Add</button>
+      </form>
+      <div v-for="task in tasks">
+        <input type="checkbox" v-model="task.completed" @change="saveTask(task)" />
+        <input v-model="task.title" />
+        <button @click="saveTask(task)">Save</button>
+        <button @click="deleteTask(task)">Delete</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
